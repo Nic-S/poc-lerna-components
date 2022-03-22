95045f8f5d4bc189980c59f4d85f4098bafb3226
Nicola.Simonetti <nicola.simonetti@dedagroup.it>
2022-03-22 23:08:01 +0100

    .npmrc

--- packages/CodeWithDescription/dist/cjs/CodeWithDesription.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export interface CodeAndDescriptionProps {
-    separator?: string;
-    code: string;
-    description: string;
-}
-export declare function CodeWithDescription({ separator, code, description }: CodeAndDescriptionProps): JSX.Element;
--- packages/ConcentricCircleIcon/dist/esm/ConcentricCricleIcon.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export interface Props {
-    innerIcon: any;
-    onClick?: () => void;
-    size: 'medium' | 'small';
-}
-export declare function ConcentricCircleIcon({ innerIcon, onClick, size }: Props): JSX.Element;
